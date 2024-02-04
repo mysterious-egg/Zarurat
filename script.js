3eda5aee5ad6fc75f1f8d920d4fc40adbe9dd691
Manvesh Thakur <manveshthakur0102@gmail.com>
2024-02-05 03:02:17 +0530

    Added

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('.loco'),
     smooth: true
 });
+function redirectToDiv() {
+    // Redirect to the div with smooth scroll (optional)
+    document.getElementsByClassName('cta').scrollIntoView({ behavior: 'smooth' });
+}
